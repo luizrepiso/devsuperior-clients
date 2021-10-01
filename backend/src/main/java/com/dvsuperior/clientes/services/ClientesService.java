9ac12740ea07b2a027009d8adf3ca7d7b810a030
luizrepiso@yahoo.es <90225213+luizrepiso@users.noreply.github.com>
2021-09-30 21:39:12 -0300

    Database seeding

--- backend/src/main/java/com/dvsuperior/clientes/services/ClientesService.java
+++ backend/src/main/java/com/dvsuperior/clientes/services/ClientesService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dvsuperior.clientes.entities.Clientes;
 import com.dvsuperior.clientes.repositories.ClientesRepository;
@@ -14,6 +15,7 @@ public class ClientesService {
 	@Autowired
 	private ClientesRepository repository;
 
+	@Transactional(readOnly = true)
 	public List<Clientes> findAll() {
 		return repository.findAll();
 
