aa16b304a4bf19d6d522119e957cda8eec7c7508
luizrepiso@yahoo.es <90225213+luizrepiso@users.noreply.github.com>
2021-10-01 20:28:57 -0300

    POST Clientes

--- backend/src/main/java/com/dvsuperior/clientes/resources/ClientesResource.java
+++ backend/src/main/java/com/dvsuperior/clientes/resources/ClientesResource.java
@@ -1,13 +1,17 @@
 package com.dvsuperior.clientes.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dvsuperior.clientes.dto.ClientesDTO;
 import com.dvsuperior.clientes.services.ClientesService;
@@ -24,10 +28,19 @@ public class ClientesResource {
 		List<ClientesDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ClientesDTO> findById(@PathVariable Long id) {
 		ClientesDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+
+	@PostMapping
+	public ResponseEntity<ClientesDTO> insert(@RequestBody ClientesDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+				return ResponseEntity.created(uri).body(dto);
+		
 	}
 }
--- backend/src/main/java/com/dvsuperior/clientes/services/ClientesService.java
+++ backend/src/main/java/com/dvsuperior/clientes/services/ClientesService.java
@@ -25,11 +25,20 @@ public class ClientesService {
 		return list.stream().map(x -> new ClientesDTO(x)).collect(Collectors.toList());
 
 	}
+
 	@Transactional(readOnly = true)
 	public ClientesDTO findById(Long id) {
 		Optional<Clientes> obj = repository.findById(id);
 		Clientes entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
-		return new ClientesDTO (entity);
-				
+		return new ClientesDTO(entity);
+	}
+
+	@Transactional
+	public ClientesDTO insert(ClientesDTO dto) {
+		Clientes entity = new Clientes();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new ClientesDTO(entity);
+
 	}
 }
